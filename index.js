6fc75558c69f5fe8ea0c139c1f1a44af0e448126
Anton Pretorius <antonjpster@gmail.com>
2024-10-28 11:27:38 +0200

    add index to repo

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("LMAO")
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+<body>
+    <h1>My first github repo commit:</h1>
+</body>
+</html>
